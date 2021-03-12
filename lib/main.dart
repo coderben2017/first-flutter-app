6eae7f42e5ad7c309bd06e244f32df95c6262ddc
coderben <coderben2016@126.com>
2021-03-12 16:33:11 +0800

    完成codelab2

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,8 @@ import 'package:english_words/english_words.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: RandomWords());
+    return MaterialApp(
+        theme: ThemeData(primaryColor: Colors.green), home: RandomWords());
   }
 }
 
@@ -15,6 +16,7 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = [];
+  final Set<WordPair> _saved = new Set<WordPair>();
   final textStyle = TextStyle(fontSize: 18);
 
   Widget _buildSuggestions() {
@@ -33,14 +35,58 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair wordPair) {
+    final bool _isSaved = _saved.contains(wordPair);
     return ListTile(
       title: Text(wordPair.asPascalCase, style: textStyle),
+      trailing: Icon(
+        _isSaved ? Icons.favorite : Icons.favorite_border,
+        color: _isSaved ? Colors.red : Colors.black,
+      ),
+      onTap: () {
+        setState(() {
+          if (_isSaved) {
+            _saved.remove(wordPair);
+          } else {
+            _saved.add(wordPair);
+          }
+        });
+      },
     );
+  }
+
+  void _handleFavoritesShow() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      final Iterable<ListTile> _listTiles = _saved.map((WordPair wordPair) {
+        return ListTile(
+          title: Text(wordPair.asPascalCase, style: textStyle),
+        );
+      });
+      final List<Widget> _dividedListTiles =
+          ListTile.divideTiles(tiles: _listTiles, context: context).toList();
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('已收藏列表'),
+        ),
+        body: ListView(
+          children: _dividedListTiles,
+        ),
+      );
+    }));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('单词列表'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: _handleFavoritesShow,
+          )
+        ],
+      ),
       body: _buildSuggestions(),
     );
   }
